7e2e5dcdca8519d829e3a6938a124e8051057bec
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-05-12 18:11:45 +0200

    access problem

--- js/App.js
+++ js/App.js
@@ -1,12 +1,4 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
-}
+/* mustache */
 
 function generateTemplate(name, data, basicElement) {
   	var template = document.getElementById(name).innerHTML;
@@ -17,21 +9,33 @@ function generateTemplate(name, data, basicElement) {
   
   	return element;
 }
+// end
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '4002',
+  'X-Auth-Token': '477122330d2acc5ec22f064016ba7dee'
+};
 
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
+fetch(baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+  });
+  
+  function setupColumns(columns) {
+	columns.forEach(function (column) {  
+		var col = new Column(column.id, column.name);
+	  	board.addColumn(col);
+	  	setupCards(col, column.cards);
+	});
+  }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
+  function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    	var cardObj = new Card(card.id, card.name);
+  		col.addCard(cardObj);
+	});
+}
--- js/Column.js
+++ js/Column.js
@@ -1,8 +1,8 @@
-function Column(name) {
+function Column(id, name) {
   	var self = this;
 
-  	this.id = randomString();
-  	this.name = name;
+  	this.id = id;
+  	this.name = name || 'No name given';
   	this.element = generateTemplate('column-template', { name: this.name, id: this.id });
 
   	this.element.querySelector('.column').addEventListener('click', function (event) {
@@ -10,9 +10,42 @@ function Column(name) {
 	      	self.removeColumn();
 	    }
 	
-	    if (event.target.classList.contains('add-card')) {
-	      	self.addCard(new Card(prompt("Enter the name of the card")));
-	    }
+			if (event.target.classList.contains('add-card')) {
+				var cardName = prompt("Enter the name of the card");
+				event.preventDefault();
+			
+				fetch(baseUrl + '/card', {
+						method: 'POST',
+						body: {
+							//body query
+						}
+					})
+					.then(function(res) {
+						return res.json();
+					})
+					.then(function() {
+						//create a new client side card
+
+						var data = new FormData();
+						data.append('name', cardName);
+						data.append('bootcamp_kanban_column_id', self.id);
+
+						fetch(baseUrl + '/card', {
+								method: 'POST',
+								headers: myHeaders,
+								body: data,
+							})
+							.then(function(res) {
+								return res.json();
+							})
+							.then(function(resp) {
+								var card = new Card(resp.id, cardName);
+								self.addCard(card);
+							});
+					});
+			
+				self.addCard(new Card(cardName));
+			}
 	});
 }
 
@@ -21,6 +54,15 @@ Column.prototype = {
 	  this.element.querySelector('ul').appendChild(card.element);
 	},
 	removeColumn: function() {
-	  this.element.parentNode.removeChild(this.element);
+		var self = this;
+		fetch(baseUrl + '/column/' + self.id, { method: 'DELETE', headers: myHeaders })
+			.then(function(resp) {
+				return resp.json();
+			})
+			.then(function(resp) {
+				self.element.parentNode.removeChild(self.element);
+			});
 	}
 };
+
+
